11451d4672538fe7f7c7002b62c53a29868d2bec
Uujnas <sanjubasnet036@gmail.com>
2021-05-17 08:51:25 +0545

    Concept of Active storage for file upload

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -44,7 +44,7 @@ class PostsController < ApplicationController
         private
 
         def post_params
-            params.require(:post).permit(:title, :body)
+            params.require(:post).permit(:title, :body, :file)
            end
     end
 
